03bfa3d49b49792068590589c0053fdf28e33036
Yqzi <y.qazi12@gmail.com>
2025-04-28 19:12:25 -0600

    stepper code

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,36 @@
+//www.elegoo.com
+//2018.10.25
+
+/*
+  Stepper Motor Control - one revolution
+
+  This program drives a unipolar or bipolar stepper motor.
+  The motor is attached to digital pins 8 - 11 of the Arduino.
+
+  The motor should revolve one revolution in one direction, then
+  one revolution in the other direction.
+
+*/
+
 #include <Arduino.h>
+#include <Stepper.h>
 
-#define ENABLE 5
-#define DIRA 3
-#define DIRB 4
+const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
+const int rolePerMinute = 17;         // Adjustable range of 28BYJ-48 stepper is 0~17 rpm
 
-int i;
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
+
 void setup() {
-  pinMode(ENABLE,OUTPUT);
-  pinMode(DIRA,OUTPUT);
-  pinMode(DIRB,OUTPUT);
+  myStepper.setSpeed(rolePerMinute);
+  // initialize the serial port:
   Serial.begin(9600);
 }
 
 void loop() {
   Serial.println("Start The Motors!");
-  //---PWM example, full speed then slow
-
-  for (i=255;i>100;i--) {
-  digitalWrite(DIRA,HIGH); //one way
-  digitalWrite(DIRB,LOW);
-  analogWrite(ENABLE,i); //enable on
-  delay(200);
-  }
-  for (i=100;i<255;i++) {
-  digitalWrite(DIRA,HIGH); //one way
-  digitalWrite(DIRB,LOW);
-  analogWrite(ENABLE,i); //enable on
-  delay(200);  
-  }
+  myStepper.step(stepsPerRevolution);
+  delay(500);
+  myStepper.step(-stepsPerRevolution);
+  delay(500);
 }
