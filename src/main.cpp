32581a514074c6143ed7de2556b1c5f0aeb606c4
Yqzi <y.qazi12@gmail.com>
2025-04-28 18:12:06 -0600

    fan motor

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include <Arduino.h>
+
+#define ENABLE 5
+#define DIRA 3
+#define DIRB 4
+
+int i;
+void setup() {
+  pinMode(ENABLE,OUTPUT);
+  pinMode(DIRA,OUTPUT);
+  pinMode(DIRB,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Start The Motors!");
+  //---PWM example, full speed then slow
+
+  for (i=255;i>100;i--) {
+  digitalWrite(DIRA,HIGH); //one way
+  digitalWrite(DIRB,LOW);
+  analogWrite(ENABLE,i); //enable on
+  delay(200);
+  }
+  for (i=100;i<255;i++) {
+  digitalWrite(DIRA,HIGH); //one way
+  digitalWrite(DIRB,LOW);
+  analogWrite(ENABLE,i); //enable on
+  delay(200);  
+  }
+}
